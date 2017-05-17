1484196f673987e7d1dfacffd56fee16e3c45a48
Michael Wender <michael@michaelwender.com>
2017-05-17 08:52:25 -0400

    First commit, code generated by `wp scaffold plugin`

---
+++ wpplugin-update-from-github.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Update from Github
+ * Plugin URI:      https://github.com/mwender/wpplugin-update-from-github
+ * Description:     A WordPress starter plugin with built-in updating from Github
+ * Author:          Michael Wender
+ * Author URI:      https://mwender.com
+ * Text Domain:     wpplugin-update-from-github
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wpplugin_Update_From_Github
+ */
